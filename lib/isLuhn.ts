5264b7783bca7f7277aba2084226688e7f2a38e4
DALE <dale_a@piscine-mars-a>
2024-03-28 13:09:34 +0000

    correct first line

--- lib/isLuhn.ts
+++ lib/isLuhn.ts
@@ -1,5 +1,5 @@
 export function isLuhn(userNb: string) {
-  let nbDeLuhn: number = +userNb % 10
+  let nbDeLuhn: number = +userNb[userNb.length-1];
 
   for (let i = userNb.length - 2; i >= 0; i--) {
     let lastNumber: number = Number(userNb[i])
