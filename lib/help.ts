868fd9c1e6e7484e3a16c90cc6cdbfd7eaaef38b
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-03-28 14:00:24 +0100

    add showGenerateHelp function

--- lib/help.ts
+++ lib/help.ts
@@ -10,5 +10,23 @@ export function showHelp() {
   console.log('Options:')
   console.log('  -c <number>\tvalidate the given number')
   console.log('  -f <number>\tcomplete the given number')
+  console.log(
+    '  -g [options] \tgenerate valid IMEI number and credit cards number (Visa, American Express and Mastercard)',
+  )
+  console.log('  -h         \tdisplay help for the command')
+}
+
+export function showGeneratehelp() {
+  console.log('\nUsage: tsx luhn.ts -g [options]\n')
+  console.log(
+    'Validate a number based on Luhn algorithm and/or complete the given number to be valid according to the Luhn algorithm.\nMore than one occurrence of an option can be passed to the command.\n',
+  )
+  console.log('Example:')
+  console.log(
+    '  tsx luhn.ts -c 5140256942783546 -c 5140256942783646 -f 192924593889831\n',
+  )
+  console.log('Commands:')
+  console.log('  imei <number>\tvalidate the given number')
+  console.log('  card <number>\tcomplete the given number')
   console.log('  -h         \tdisplay help for the command')
 }
